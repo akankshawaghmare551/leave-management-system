509477780c12d8e133e37ac906bd63d6e0a2816f
akankshawaghmare551 <akankshawaghmare551@gmail.com>
2024-06-29 18:53:10 +1100

    header and footer and index code

---
+++ admin/index1.php
@@ -0,0 +1,20 @@
+<?php
+    include '../templates/header.php'
+?>
+<div class="container">
+    <h1>Leave Management System</h1>
+    
+</div>
+
+
+
+
+
+
+
+
+
+
+<?php
+    include '../templates/footer.php'
+?>
---
+++ templates/header.php
@@ -0,0 +1,16 @@
+
+<?php 
+    $server_name = $_SERVER['SERVER_NAME'];
+    $base = "http://".$server_name.":121/leave-management-system";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>leave-management-system</title>
+    <link rel="stylesheet" href="<?php echo $base; ?>/assets/css/bootstrap.min.css">
+    <script src="<?php echo $base; ?>/assets/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    
